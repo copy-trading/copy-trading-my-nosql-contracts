16724967a9451ad40902acd75f91e36b14225d33
amigin <amigin@gmail.com>
2024-03-09 13:59:47 +0200

    Fixes

--- src/product_settings/product_settings.rs
+++ src/product_settings/product_settings.rs
@@ -2,7 +2,7 @@ use crate::TradingEngineSettingsModel;
 
 service_sdk::macros::use_my_no_sql_entity!();
 
-#[enum_of_my_no_sql_entity(table_name:"product-settings", generate_unwraps)]
+#[enum_of_my_no_sql_entity(table_name:"ct-product-settings", generate_unwraps)]
 pub enum ProductSettings {
     TradingEngineSettings(TradingEngineSettingsModel),
 }
--- src/product_settings/trading_engine_settings.rs
+++ src/product_settings/trading_engine_settings.rs
@@ -2,7 +2,7 @@ use serde::*;
 
 service_sdk::macros::use_my_no_sql_entity!();
 
-#[enum_model(partition_key:"emails", row_key: "send-grid")]
+#[enum_model(partition_key:"trading-engine", row_key: "trading-groups")]
 #[derive(Serialize, Deserialize, Clone)]
 pub struct TradingEngineSettingsModel {
     pub provider_trading_group: String,
