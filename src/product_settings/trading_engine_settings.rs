8a35fe2dc883b4240174683d642c71ec518777bd
amigin <amigin@gmail.com>
2024-03-14 10:56:37 +0200

    Get MinVolumeCopy

--- src/product_settings/trading_engine_settings.rs
+++ src/product_settings/trading_engine_settings.rs
@@ -7,4 +7,11 @@ service_sdk::macros::use_my_no_sql_entity!();
 pub struct TradingEngineSettingsModel {
     pub provider_trading_group: String,
     pub subscriber_trading_group: String,
+    pub min_volume_copy: Option<f64>,
+}
+
+impl TradingEngineSettingsModel {
+    pub fn get_min_volume_copy(&self) -> f64 {
+        self.min_volume_copy.unwrap_or(0.1)
+    }
 }
