109f86886c7fd093cdc4bb39e24363385c1e922a
amigin <amigin@gmail.com>
2024-03-14 23:41:14 +0200

    Max_subscribers_to_register

--- src/product_settings/trading_engine_settings.rs
+++ src/product_settings/trading_engine_settings.rs
@@ -8,10 +8,20 @@ pub struct TradingEngineSettingsModel {
     pub provider_trading_group: String,
     pub subscriber_trading_group: String,
     pub min_volume_copy: Option<f64>,
+    pub max_providers_to_register: Option<i32>,
+    pub max_subscribers_to_register: Option<i32>,
 }
 
 impl TradingEngineSettingsModel {
     pub fn get_min_volume_copy(&self) -> f64 {
         self.min_volume_copy.unwrap_or(0.1)
+    }
+
+    pub fn get_max_providers_to_register(&self) -> i32 {
+        self.max_providers_to_register.unwrap_or(3)
+    }
+
+    pub fn get_max_subscribers_to_register(&self) -> i32 {
+        self.max_subscribers_to_register.unwrap_or(3)
     }
 }
