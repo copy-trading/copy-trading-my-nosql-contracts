c15b4f353c5530102e31cf6c760920455eed9e10
amigin <amigin@gmail.com>
2024-03-15 13:16:43 +0200

    AddedCopyTradingGroups

--- src/product_settings/product_settings.rs
+++ src/product_settings/product_settings.rs
@@ -1,8 +1,9 @@
-use crate::TradingEngineSettingsModel;
+use crate::{CopyTradingGroup, TradingEngineSettingsModel};
 
 service_sdk::macros::use_my_no_sql_entity!();
 
 #[enum_of_my_no_sql_entity(table_name:"ct-product-settings", generate_unwraps)]
 pub enum ProductSettings {
     TradingEngineSettings(TradingEngineSettingsModel),
+    CopyTradingGroups(CopyTradingGroup),
 }
